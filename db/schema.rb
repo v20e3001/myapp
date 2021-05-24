9bd450ad0a0b7a4724ecd4ae069165f32d6b8f0d
Kentaro Iwasaki <v20e3001@oita-u.ac.jp>
2021-05-24 07:42:25 +0000

    Memoの削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20210518054655) do
 
-  create_table "memos", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
