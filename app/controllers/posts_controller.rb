92b47a48190a1acb316a9605b69838fc30643131
Kentaro Iwasaki <v20e3001@oita-u.ac.jp>
2021-06-12 10:12:33 +0000

    Lesson08の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
     
+    def index
+        @posts = Post.all.order(created_at: 'desc')
+        render 'posts/index'
+    end
+    
     def show
         @post = Post.find(params[:id])
     end
+    
+    def new
+        
+    end
+    
 end
