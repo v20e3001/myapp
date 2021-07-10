824bf1df6bc1da987149228666412925bce1d590
Kentaro Iwasaki <v20e3001@oita-u.ac.jp>
2021-07-10 11:56:28 +0000

    Lesson12の課題

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@
 #  updated_at :datetime         not null
 #
 class Post < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     # タイトル:空文字禁止、最低３文字以上
     validates :title, presence: true, length: {minimum: 3}
     # body :空文字禁止
